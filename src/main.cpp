812245b9518212fe9078aba4e3710d747e98886e
ilPedar <mascherpa.pietro@gmail.com>
2022-04-19 23:42:23 +0200

    MODIFICHE PER DEFINITIVO2

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,34 @@
 
 // Simple sketch to start a connection as client.
 
+#define PING_PIN 3
+#define ECHO_PIN 4
+
+long microsecondsToCentimeters(long microseconds) { //libreria per trasformare da microsecondi a centimetri (da mettere fuori dal loop)
+   return microseconds / 29 / 2;
+}
+
+long getMeasurement()     //libreria che prende la misura per ottenere il risultato dall'ultrasuoni. 
+{
+   Serial.print("Taking measurement...");
+   long duration, cm;
+   pinMode(PING_PIN, OUTPUT);
+   digitalWrite(PING_PIN, LOW);
+   delayMicroseconds(2);
+   digitalWrite(PING_PIN, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(PING_PIN, LOW);
+   pinMode(ECHO_PIN, INPUT);
+   duration = pulseIn(ECHO_PIN, HIGH);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+   delay(100);
+   return cm;
+}
+
+
 InetGSM inet;
 // CallGSM call;
 // SMSGSM sms;
@@ -19,13 +47,21 @@ InetGSM inet;
 char mesg[50];
 int numdata;
 boolean started = false;
-long h = 30;
+long h = 0;
 
 String THINGSPEAK_API_KEY = "G44TANS8WMVAW8SH";
 
 
 void setup()
 {
+   pinMode(9, OUTPUT);      //turn on the module form program è necesario saldare il jumper R13 sulla scheda e fa tutto lui. 
+   digitalWrite(9,LOW);
+   delay(1000);
+   digitalWrite(9,HIGH);
+   delay(2000);
+   digitalWrite(9,LOW);
+   delay(3000);            //turn on the module form program qui finisce la parte necessaria per accentere il modulo appena si alimenta la scheda, può essere usato anche nel programma per accendere e spegnere
+   
   // Serial connection.
   Serial.begin(9600);
   Serial.println("GSM Shield testing.");
@@ -45,7 +81,7 @@ void setup()
   {
     // GPRS attach, put in order APN, username and password.
     // If no needed auth let them blank.
-    if (inet.attachGPRS("ibox.tim.it", "", ""))
+    if (inet.attachGPRS("TM", "", ""))
       Serial.println("status=ATTACHED");
     else
       Serial.println("status=ERROR");
@@ -57,35 +93,18 @@ void setup()
     // Read until serial buffer is empty.
     gsm.WhileSimpleRead();
 
+    h=getMeasurement();// take the measurement
+
     // TCP Client GET, send a GET request to the server and
     // save the reply.
 
-        numdata=inet.httpGET("api.thingspeak.com", 80, "/update?api_key=G44TANS8WMVAW8SH&field1=15", mesg, 50);
+        numdata=inet.httpGET("api.thingspeak.com", 80, "/update?api_key=G44TANS8WMVAW8SH&field1=15", mesg, h);
         //Print the results.
         Serial.println("\nNumber of data received:");
         Serial.println(numdata);
         Serial.println("\nData received:");
         Serial.println(mesg);
-    
-    
-
-   // inet.connectTCP("api.thingspeak.com", 80);
-
-  /* Serial.println("tcp");
-    char const* uri0 = ("https://api.thingspeak.com/update?api_key=" + THINGSPEAK_API_KEY + "&field1=" + String(h)).c_str();
-    Serial.println("I am alive");
-    Serial.println(uri0);
-    char response[200];
-    // inet.println(str);//begin send data to remote server
-    Serial.println("I am still alive");
-
-    char const* uri1 = ("/update?api_key=" + THINGSPEAK_API_KEY + "&field1=4").c_str();
-    Serial.println("Am I immortal?!");
-    Serial.println(uri1);
-    inet.httpGET("http://api.thingspeak.com", 80, uri1, response, 200);
-    Serial.println(response);
-    Serial.println("Zio Pippo");
-  */  
+      
   }
 };
 
